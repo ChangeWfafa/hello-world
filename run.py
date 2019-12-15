662f2a4f9cd1ed3e414536b565f8767bb6a8798a
lsj985 <54632196+lsj985@users.noreply.github.com>
2019-12-15 20:27:34 +0800

    Update run.py

--- run.py
+++ run.py
@@ -12,14 +12,6 @@ import matplotlib.pyplot as plt
 
 import torch.nn as nn
 
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.lin1 = nn.Linear(28*28,10)
-    def forward(self,x):
-        x = self.lin1(x)
-
-        return x
 
 
 
